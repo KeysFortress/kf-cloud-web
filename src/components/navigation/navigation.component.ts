2ffbdaee8889a7d02ed7fb644d2931921ce0bae9
Kristifor Milchev <km@airzen.io>
2024-06-07 11:30:54 +0300

    Adding the events to navigate the internal pages.

--- src/components/navigation/navigation.component.ts
+++ src/components/navigation/navigation.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { ProfileExpandableComponent } from '../profile-expandable/profile-expandable.component';
-import { TopNavComponent } from '../top-nav/top-nav.component';
+import { Component } from "@angular/core";
+import { ProfileExpandableComponent } from "../profile-expandable/profile-expandable.component";
+import { TopNavComponent } from "../top-nav/top-nav.component";
+import { UtilsService } from "../../../helpers/utils.service";
 
 @Component({
-  selector: 'app-navigation',
+  selector: "app-navigation",
   standalone: true,
-  templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css',
+  templateUrl: "./navigation.component.html",
+  styleUrl: "./navigation.component.css",
   imports: [ProfileExpandableComponent, TopNavComponent],
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  /**
+   *
+   */
+  constructor(public utils: UtilsService) {}
+}
