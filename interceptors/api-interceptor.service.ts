5f7ef6e1bc6b9e51d97729da81ebeae07130878f
Kristifor Milchev <km@airzen.io>
2024-06-09 11:14:37 +0300

    updating the api url.

--- interceptors/api-interceptor.service.ts
+++ interceptors/api-interceptor.service.ts
@@ -4,7 +4,7 @@ export const urlInterceptor: HttpInterceptorFn = (req, next) => {
   console.log("Intercepting request");
   // Modify the request URL here
   const modifiedRequest = req.clone({
-    url: `https://kristifor.kidswlan.de/${req.url}`,
+    url: `http://localhost:8080/${req.url}`,
   });
 
   return next(modifiedRequest);
@@ -13,9 +13,7 @@ export const urlInterceptor: HttpInterceptorFn = (req, next) => {
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authToken = "YOUR_AUTH_TOKEN_HERE";
 
-  const authReq = req.clone({
- 
-  });
+  const authReq = req.clone({});
 
   return next(authReq);
 };
